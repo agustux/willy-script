5f2dc4dffc0fa8cee5793ef932b9df56722235c9
gus <gus@ubuntu>
2025-07-23 12:19:53 -0500

    purged orca too (unneeded)

--- willy-script.sh
+++ willy-script.sh
@@ -11,7 +11,7 @@ sudo rm /swap.img
 sudo sed -i '/^\/swap\.img/s/^/#/' /etc/fstab
 
 # Purging Useless GNOME utilities:
-sudo apt purge software-properties-* gnome-font-viewer update-manager gnome-remote-desktop gnome-user-docs gnome-characters gnome-logs -y
+sudo apt purge software-properties-* gnome-font-viewer update-manager gnome-remote-desktop gnome-user-docs gnome-characters gnome-logs orca -y
 sudo apt autoremove --purge -y
 
 # Misc:
